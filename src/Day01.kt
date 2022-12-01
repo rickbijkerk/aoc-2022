efaff4489a4b703805e4d8685b3f04483f8ae916
Rick Bijkerk <rickbijkerk@bol.com>
2022-12-01 11:37:54 +0100

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,53 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val calories = mutableListOf<Int>()
+
+        val newLines = input.withIndex().filter { x -> x.value.isEmpty() }.map { it.index }.toMutableList()
+        newLines.add(input.size)
+
+        var start = 0
+        newLines.map { index ->
+            val caloriesAsInt = input.subList(start + 1, index).map { stringValue -> stringValue.toInt() }
+            val sumOfCalories = caloriesAsInt.sumOf { calorie -> calorie }
+            start = index
+
+            calories.add(sumOfCalories)
+        }
+
+        return calories.max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val calories = mutableListOf<Int>()
+
+        val newLines = input.withIndex().filter { x -> x.value.isEmpty() }.map { it.index }.toMutableList()
+        newLines.add(input.size)
+
+        var start = 0
+
+        newLines.map { index ->
+            val caloriesAsInt = input.subList(start + 1, index).map { stringValue -> stringValue.toInt() }
+            val sumOfCalories = caloriesAsInt.sumOf { calorie -> calorie }
+            start = index
+
+            calories.add(sumOfCalories)
+        }
+
+        return calories.toMutableList().sortedDescending().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+
+    val result = part1(testInput)
+    val expected = 24000
+    println("test result: $result")
+    println("test expected:$expected \n")
+
+    check(result == expected)
+
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println("Part1 ${part1(input)}")
+    println("Part2 ${part2(input)}")
 }
